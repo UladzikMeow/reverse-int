8e64a7cc62a7d083875f2952997fffb13d2272e9
UladzikMeow <vladislav.gribanov2302@gmail.com>
2021-03-13 22:20:36 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n >= 0) {
+        let arrN = n.toString().split("");
+        let newArr = [];
+        console.log(arrN);
+        for (let i = arrN.length - 1; i >= 0; i--) {
+            newArr.push(arrN[i]);
+        }
+        return Number(newArr.join(""));
+    } else {
+        n = n * -1;
+        let arrN = n.toString().split("");
+        let newArr = [];
+        console.log(arrN);
+        for (let i = arrN.length - 1; i >= 0; i--) {
+            newArr.push(arrN[i]);
+        }
+        return Number(newArr.join(""));
+    }
+};
